3fda497c8f0bf2e7e816c354e283dd9b0e709672
mindtreefeb86 <mindtreefeb86@MINDALLB378>
2022-06-27 14:26:33 +0530

    Add project files.

---
+++ DatabaseTesting/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DatabaseTesting
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=MINDALLB378;Initial Catalog=students;User ID=sa;Password=pass@word1");
+            SqlCommand cmd = new SqlCommand("select * from StudentDetails", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
